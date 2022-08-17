5669f31c8cbb67898abe9a39dfc9cd5358a8f601
Mohamed Hefny <hefny.7@gmail.com>
2022-08-17 08:44:17 +0200

    Detect tiktok app is opened.

--- app/src/main/java/com/mohamedhefny/tiktokblocker/services/AppsMonitoringService.kt
+++ app/src/main/java/com/mohamedhefny/tiktokblocker/services/AppsMonitoringService.kt
@@ -1,9 +1,11 @@
 package com.mohamedhefny.tiktokblocker.services
 
 import android.accessibilityservice.AccessibilityService
+import android.app.ActivityManager
 import android.util.Log
 import android.view.KeyEvent
 import android.view.accessibility.AccessibilityEvent
+import com.mohamedhefny.tiktokblocker.R
 
 class AppsMonitoringService : AccessibilityService() {
 
@@ -11,13 +13,21 @@ class AppsMonitoringService : AccessibilityService() {
         private const val TAG = "ActiveAppServiceTAG"
     }
 
+    private lateinit var activityManager: ActivityManager
+    private lateinit var tiktokPackageName: String
+
     override fun onServiceConnected() {
         super.onServiceConnected()
         Log.d(TAG, "Accessibility service connected")
+        activityManager = applicationContext.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        tiktokPackageName = applicationContext.getString(R.string.tiktok_package_name)
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         Log.d(TAG, "Accessibility event $event")
+
+        if (event?.packageName != tiktokPackageName)
+            return
     }
 
     override fun onKeyEvent(event: KeyEvent?): Boolean {
